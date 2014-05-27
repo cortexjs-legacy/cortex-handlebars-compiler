82a0e8aab1a3abc9d3a2ffd321ad62d516752c22
kaelzhang <i@kael.me>
2014-05-27 21:45:27 +0800

    adds test fixtures and cases

--- test/cortex-handlebars-compiler.js
+++ test/cortex-handlebars-compiler.js
@@ -1,4 +1,32 @@
 'use strict';
 
 var expect = require('chai').expect;
-var cortex_handlebars_compiler = require('../');
+var compiler = require('../');
+var fs = require('fs');
+var jf = require('jsonfile');
+var node_path = require('path');
+
+var template = '<html><head></head><body>{{{facade "cortex-hybrid-sample"}}}</body></html>';
+var fixtures = node_path.join(__dirname, 'fixtures');
+var cortex_json = node_path.join(fixtures, 'cortex.json');
+var shrinkwrap_json = node_path.join(fixtures, 'cortex-shrinkwrap.json');
+var expected_html = node_path.join(fixtures, 'expected.html');
+
+var pkg = jf.readFileSync(cortex_json);
+var shrinkwrap = jf.readFileSync(shrinkwrap_json);
+var expected = fs.readFileSync(expected_html).toString();
+
+var c = compiler({
+  pkg: pkg,
+  shrinkWrap: shrinkwrap,
+  root: '../../../',
+  ext: '.min.js'
+});
+
+
+describe("compiler()", function(){
+  it(".compile(template)()", function(){
+    var fn = c.compile(template);
+    expect(fn()).to.equal(expected);
+  });
+});
