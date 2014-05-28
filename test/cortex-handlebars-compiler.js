bea53760d33653c3f0e46b899866d1bc23607f71
kaelzhang <i@kael.me>
2014-05-28 18:06:42 +0800

    updates test cases and fixtures; fixes #1

--- test/cortex-handlebars-compiler.js
+++ test/cortex-handlebars-compiler.js
@@ -6,27 +6,57 @@ var fs = require('fs');
 var jf = require('jsonfile');
 var node_path = require('path');
 
-var template = '<html><head></head><body>{{{facade "cortex-hybrid-sample"}}}</body></html>';
 var fixtures = node_path.join(__dirname, 'fixtures');
 var cortex_json = node_path.join(fixtures, 'cortex.json');
 var shrinkwrap_json = node_path.join(fixtures, 'cortex-shrinkwrap.json');
-var expected_html = node_path.join(fixtures, 'expected.html');
 
 var pkg = jf.readFileSync(cortex_json);
 var shrinkwrap = jf.readFileSync(shrinkwrap_json);
-var expected = fs.readFileSync(expected_html).toString();
-
-var c = compiler({
-  pkg: pkg,
-  shrinkWrap: shrinkwrap,
-  root: '../../../',
-  ext: '.min.js'
-});
-
 
 describe("compiler()", function(){
-  it(".compile(template)()", function(){
-    var fn = c.compile(template);
-    expect(fn()).to.equal(expected);
+  [
+    {
+      name: 'normal'
+    },
+    {
+      name: 'single'
+    },
+    {
+      name: 'more'
+    },
+    {
+      name: 'range-not-found',
+      throw: true
+    }
+  ].forEach(function (c) {
+    var template_file = node_path.join(fixtures, c.name + '.template');
+    var template = fs.readFileSync(template_file).toString();
+
+    it(template, function(){
+      var cp = compiler({
+        pkg: pkg,
+        shrinkWrap: shrinkwrap,
+        root: '../../../',
+        ext: '.min.js'
+      });
+
+      var compiled;
+      var error;
+      try {
+        compiled = cp.compile(template)();
+      } catch(e) {
+        error = e;
+      }
+
+      if (c.throw) {
+        expect(error).not.to.equal();
+        return;
+      }
+
+      var expected_file = node_path.join(fixtures, c.name + '.expected.html');
+      var expected = fs.readFileSync(expected_file).toString();
+
+      expect(compiled).to.equal(expected);
+    });
   });
 });
