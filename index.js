0d6df0b3798abadd93dc8c6ebd46997a98d5838c
supersheep <supersheepxjy@gmail.com>
2015-04-09 11:47:43 +0800

    better error report

--- index.js
+++ index.js
@@ -145,7 +145,14 @@ Compiler.prototype._modfile_handler = function(title, options) {
   var name = obj.name;
   var range = obj.range || '*';
   var path = obj.path;
+  if(!versions[name]){
+    throw new Error("Module \"" + name + "\" not found, please install first.");
+  }
+
   var version = semver.maxSatisfying(versions[name], range);
+  if(!version){
+    throw new Error("Valid version not found for module " + name + "@" + range);
+  }
 
   if(path == ''){
     path = name + '.js';
